5fb10123b70e53e4c8660dcac1557ac6e81b9000
stackgen-automation <automation@stackgen.com>
2024-10-09 16:42:34 +0000

    Automated commit

--- gitops/terraform/outputs.tf
+++ gitops/terraform/outputs.tf
@@ -1,12 +1,12 @@
 
-output "aws_iam_role_dogeapi_role_arn" {
-  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.arn
+output "aws_iam_role_dogeapi_role_name" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.name
   sensitive = false
 }
 
 
-output "aws_iam_role_dogeapi_role_name" {
-  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.name
+output "aws_iam_role_dogeapi_role_arn" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.arn
   sensitive = false
 }
 
