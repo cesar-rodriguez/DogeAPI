a852b5091fc6825199140c4b64cc1cb09e31b50b
stackgen-automation <automation@stackgen.com>
2024-10-09 19:12:00 +0000

    Automated commit

---
+++ gitops/terraform/outputs.tf
@@ -0,0 +1,17 @@
+
+output "aws_iam_role_dogeapi_role_arn" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_dogeapi_role_name" {
+  value = module.appcd_5946a085-5560-51f9-8683-3b21d7bd5f37.name
+  sensitive = false
+}
+
+
+output "aws_dynamodb_my_table_arn" {
+  value = module.appcd_c6b64537-fcfe-49fb-8cc7-1d15863db11d.arn
+  sensitive = false
+}
